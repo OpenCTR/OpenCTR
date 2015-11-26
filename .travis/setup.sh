2c1dbe5defa848d724ff291ac8ced2486c1402f5
Symbitic <alex.shaw.as@gmail.com>
2015-11-26 14:26:37 -0600

    More OS preping

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -4,43 +4,62 @@ set -e
 
 if [ ! "${TRAVIS}" == "true" ]
 then
-    echo "$0 can only be called inside a travis-ci environment"
+    echo "$0 can only be called inside a Travis-CI environment"
     exit 1
 fi
 
-CMAKE=$(which cmake)
-if [ -z "${CMAKE}" ]
-then
-    echo "CMake is NOT installed"
-    exit 1
-fi
-
-CMAKE_BUILD_TYPE="Release"
-
-if [ "${OS}" == "linux" ]
-then
+case "${OS}" in
+linux)
     CPACK_GENERATOR="TBZ2"
-elif [ "${OS}" == "osx" ]
-then
+    OPENCTR_HOST="Linux"
+    ;;
+osx)
     CPACK_GENERATOR="TBZ2"
-elif [ "" == "mingw" ]
-then
+    OPENCTR_HOST="OSX"
+
+    brew update
+    brew install cmake
+    brew install ninja
+    brew install gmp
+    brew install mpfr
+    brew install libmpc
+    brew install libelf
+    brew install autoconf
+    brew install automake
+    brew install bison
+    brew install flex
+    brew install libtool
+    brew install binutils
+    brew install gawk
+    brew install texinfo
+    ;;
+mingw)
+    if [ "${TRAVIS_OS_NAME}" == "osx" ]
+    then
+        echo "MinGW builds are not supported on OSX"
+        exit 1
+    fi
+
     CPACK_GENERATOR="ZIP"
-else
+    OPENCTR_HOST="Win32"
+    ;;
+*)
     echo "Unrecognized OS: ${OS}"
     exit 1
-fi
+    ;;
+esac
 
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
-${CMAKE} \
-  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+cmake \
+  -DCMAKE_BUILD_TYPE="Release" \
   -DCPACK_GENERATOR=${CPACK_GENERATOR} \
   -DENABLE_DOC=OFF \
   -DENABLE_LIBCTRU=OFF \
   -DENABLE_LIBCTR=ON \
   -DENABLE_LLVM=OFF \
   -DENABLE_GCC=ON \
+  -DOPENCTR_HOST=${OPENCTR_HOST}
   ${SOURCE_DIR}
 
