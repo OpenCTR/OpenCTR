b2470a7554fa6b803ac81b9d620e609d34efa809
Symbitic <alex.shaw.as@gmail.com>
2016-01-09 18:54:02 -0600

    Upgrade to newlib-2.3.0

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
+################################################################################
+# OpenCTR - A free and open-source SDK for Nintendo 3DS homebrew.
+#
+# Copyright (C) 2015 The OpenCTR Project.
+#
+# This file is part of OpenCTR.
+#
+# OpenCTR is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 as published by
+# the Free Software Foundation.
+#
+# OpenCTR is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenCTR. If not, see <http://www.gnu.org/licenses/>.
+################################################################################
 
 if [ ! "${TRAVIS}" == "true" ]
 then
     echo "$0 can only be called inside a Travis-CI environment"
     exit 1
+fi
+
+if [ -z "${CMAKE_BUILD_TYPE}" ]
+then
+    CMAKE_BUILD_TYPE="Release"
 fi
 
 case "${TRAVIS_OS_NAME}" in
@@ -29,11 +53,18 @@ osx)
     ;;
 esac
 
+CMAKE=$(which cmake)
+if [ ! -e "${CMAKE}" ]
+then
+    echo "CMake executable not found"
+    exit 1
+fi
+
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
 cmake \
-  -DCMAKE_BUILD_TYPE="Release" \
+  -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
   -DCPACK_GENERATOR=${CPACK_GENERATOR} \
   -DENABLE_DOC=OFF \
   -DENABLE_TOOLCHAIN=ON \
